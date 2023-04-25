7b520b8ebcd45807bd94abfe0e467d69246ccd06
OlegVazhinsky <oleg_vazhinsky92@mail.ru>
2023-04-25 23:29:27 +0700

    note_class.py created.

---
+++ note_class.py
@@ -0,0 +1,9 @@
+class Note(object):
+    def __init__(self, id, title, text, date, time, key):
+        self.id = id
+        self.title = title
+        self.text = text
+        self.date = date
+        self.time = time
+        self.key = key
+        pass
