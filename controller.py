d65a08b45a1151972874b122206265ed6891c149
OlegVazhinsky <oleg_vazhinsky92@mail.ru>
2023-04-25 22:55:51 +0700

    Fixed situation in controller.py, where 0 arguments set.

--- controller.py
+++ controller.py
@@ -2,16 +2,18 @@ from model import *
 from view import *
 
 def init(args):
-    if (args[1] == "help"):
+    if (len(args) < 2):
         print_help()
-    elif (args[1] == "add"):
-        command = "add"
-    elif (args[1] == "delete"):
-        command = "delete"
-    elif (args[1] == "edit"):
-        command = "edit"
-    elif (args[1] == "list"):
-        command = "list"
     else:
-        print_error_message(args[1])
-    
+        if (args[1] == "help"):
+            print_help()
+        elif (args[1] == "add"):
+            command = "add"
+        elif (args[1] == "delete"):
+            command = "delete"
+        elif (args[1] == "edit"):
+            command = "edit"
+        elif (args[1] == "list"):
+            command = "list"
+        else:
+            print_error_message(args[1])
