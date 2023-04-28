c3c7563e9e1c3de15e0241d87c5355c522aa4e4f
OlegVazhinsky <oleg_vazhinsky92@mail.ru>
2023-04-28 23:13:09 +0700

    view.py - edited the lexis

--- view.py
+++ view.py
@@ -3,13 +3,13 @@ def print_help():
     print("Program helps you to create and edit notes in commandline.")
     print("It uses the following sintax:\n")
     print("- to add note use:")
-    print('add -title "NOTE_TITLE" -text "NOTE_TEXT"\n')
+    print('add -title NOTE_TITLE -text NOTE_TEXT\n')
     print("- to show the list of all notes use:")
     print('list\n')
     print("- to delete note use:")
-    print('delete -id "NOTE_ID"\n')
+    print('delete -id NOTE_ID\n')
     print("- to edit note use:")
-    print('edit -id "NOTE_ID" -title "NOTE_TITLE" -text "NOTE_TEXT"\n')
+    print('edit -id NOTE_ID -title NOTE_TITLE -text NOTE_TEXT\n')
     print("If you find any bugs or you have any suggestions, please e-mail: oleg_vazhinsky92@mail.ru")
 
 def print_error_message(command):
@@ -17,8 +17,8 @@ def print_error_message(command):
     print("Please, check youself and try again.")
     print("You can also use 'help' argument to see all commands.")
 
-def print_action_error(action):
-    print("You tried to " + action + " note(s), but non of them has been created.")
+def print_action_error():
+    print("You tried to see notes, but none was created.")
 
 def print_default_message(message):
     print(message)
